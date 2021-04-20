593d9346c8eb15523d20999ca15cb38c53a5670c
Hendrik Lelis <hviniciu@everis.com>
2021-04-20 19:04:36 +0100

    Exercise 1 paggination

--- src/app/pages/exercise-one/exercise-one.component.ts
+++ src/app/pages/exercise-one/exercise-one.component.ts
@@ -9,22 +9,71 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./exercise-one.component.css'],
 })
 export class ExerciseOneComponent implements OnInit {
-  //vars
+  // vars
   usersList: Observable<User[]>;
   SERVER_URL: string = 'http://localhost:3001';
 
-  //paggination
-  page: number = 1;
-  pageLimit: number = 4;
-  totalRecords: number = 0;
+  // paggination
+  page: number = 1; // Current page
+  pageLimit: number = 4; // Limit per page
+  totalRecords: number = 0; // Total items
 
-  fetchUsers() {
-    this.usersList = this.http.get<User[]>(this.SERVER_URL + '/users/1/4');
+  /**
+   * Function to go to next page
+   */
+  nextPage() {
+    // check the current page. It must be > 1
+    if (this.page * this.pageLimit < this.totalRecords) {
+      // decrement page
+      this.page++;
+
+      // Load users again
+      this.fetchUsers();
+    }
   }
 
+  /**
+   * Function to back to the previus page
+   */
+  previousPage() {
+    // check the current page. It must be > 1
+    if (this.page > 1) {
+      // decrement page
+      this.page--;
+
+      // Load users again
+      this.fetchUsers();
+    }
+  }
+
+  /**
+   * Function to get all users from database
+   */
+  fetchUsers() {
+    
+    this.http
+      .get<number>(this.SERVER_URL + `/users/count`)
+      .subscribe((val) => this.totalRecords = val[0].count)
+
+    // get all users and put them into a observable
+    this.usersList = this.http.get<User[]>(
+      this.SERVER_URL + `/users/${this.page}/${this.pageLimit}`
+    );
+  }
+
+  /**
+   * Component constructor
+   * @param http HTTPClient
+   */
   constructor(private http: HttpClient) {}
 
+  /**
+   * Component loader
+   */
   ngOnInit(): void {
+    /**
+     * call fetUsers function to get all users
+     */
     this.fetchUsers();
   }
 }
