f818b4fd2b2ea07d2851bc8c09a5ef2041181bf6
Hendrik Lelis <hviniciu@everis.com>
2021-04-21 14:26:04 +0100

    User create and update

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,26 +1,76 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { HttpClient } from '@angular/common/http';
+declare var $: any;
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css'],
 })
-export class UserEditComponent implements OnInit {
+export class UserEditComponent implements OnChanges {
   date: string;
+  SERVER_URL: string = 'http://localhost:3001';
   // functions
   handleCreateEdit(isDelete: boolean) {
     // check if action is a delete
     if (isDelete) {
+      alert('User deleted');
     } else {
-      alert(this.action);
+      switch (this.action) {
+        case 'Create':
+          // Make patch to user
+          this.http
+            .post<number>(this.SERVER_URL + `/users/`, {
+              country: 1,
+              username: $('#username').val(),
+              image: 'default',
+              name: $('#name').val(),
+              surname: $('#surname').val(),
+              bornDate: $('#bornDate').val(),
+              phone: Number.parseInt($('#phone').val()),
+              password: '12341234',
+            })
+            .subscribe((val) => {
+              if (val['err'] && val['err'] == 'ER_DUP_ENTRY') {
+                alert('Username is aleardy in use');
+              } else {
+                alert('User created');
+              }
+            });
+
+          break;
+        case 'Edit':
+          // Make patch to user
+          this.http
+            .patch<number>(this.SERVER_URL + `/users/` + this.user.ID, {
+              country: 1,
+              username: $('#username').val(),
+              image: 'default',
+              name: $('#name').val(),
+              surname: $('#surname').val(),
+              bornDate: $('#bornDate').val(),
+              phone: Number.parseInt($('#phone').val()),
+              password: '12341234',
+            })
+            .subscribe((val) => console.log(val));
+
+          alert('User updated');
+          break;
+      }
     }
   }
 
-  constructor() {}
+  /**
+   * Component constructor
+   * @param http httpclient
+   */
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-    this.date = new Date(this.user.BornDate).toISOString().substring(0, 10);
+  ngOnChanges() {
+    if (this.action != 'Create') {
+      this.date = new Date(this.user.BornDate).toISOString().substring(0, 10);
+    }
   }
 
   @Input() action: string;
